7863f5ca5abc07822fba0dbb8c7fd2dfe763aaab
Gergő Magyar <magyargergo01@t-online.hu>
2016-12-07 22:21:13 +0100

    src/app/theme/theme.config.js

--- src/app/theme/theme.config.js
+++ src/app/theme/theme.config.js
@@ -10,14 +10,14 @@
 
   /** @ngInject */
   function config(baConfigProvider, colorHelper) {
-    //baConfigProvider.changeTheme({blur: true});
+    baConfigProvider.changeTheme({blur: true});
     //
-    //baConfigProvider.changeColors({
-    //  default: 'rgba(#000000, 0.2)',
-    //  defaultText: '#ffffff',
-    //  dashboard: {
-    //    white: '#ffffff',
-    //  },
-    //});
+    baConfigProvider.changeColors({
+      default: 'rgba(#000000, 0.2)',
+      defaultText: '#ffffff',
+      dashboard: {
+        white: '#ffffff',
+      },
+    });
   }
 })();
